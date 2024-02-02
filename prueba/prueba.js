b44844b545dea6f726bcb909279550d414ca7a2d
Lacrita24 <jhoy16ps4@gmail.com>
2024-02-02 13:10:06 -0400

    Aplicando la prueba dos

--- prueba/prueba.js
+++ prueba/prueba.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // Endpoint GET sencillo
-router.get('/', (req, res) => {
+router.get('/prueba', (req, res) => {
   console.log('Solicitud GET recibida en /prueba');
   res.send('¡Hola desde el endpoint GET de prueba muchachos!');
 });
 
+// Endpoint GET sencillo
+router.get('/prueba-dos', (req, res) => {
+    console.log('Solicitud GET recibida en /prueba-dos');
+    res.send('¡Hola desde el endpoint GET de prueba numero dos muchachos!');
+  });
 module.exports = router;
