2bb8490a0eb8bd0d16fc9fee5887ee69f5aeeadf
Edouaard7 <136937565+Edouaard7@users.noreply.github.com>
2024-02-27 15:55:51 -0400

    Creacion de Tareas Corregido

--- src/controllers/TareaController.js
+++ src/controllers/TareaController.js
@@ -58,63 +58,105 @@ class TareaController {
         }
     }
 
-    // crear una tarea
-    static async create (req, res){
-        try {
-            // capturar datos
-            const { date, start_time, end_time, id_project, id_service } = req.body
-            // comprobar si existe el proyecto
-            const project = await Proyecto.findByPk(id_project)
-            if (!project) {
-                return res.status(404).json({message: 'Proyecto no encontrado'})
-            }
-            // comprobar si existe el servicio
-            const serviceFound = await Servicio.findByPk(id_service)
-            if (!serviceFound) {
-                return res.status(404).json({message: 'Servicio no encontrado'})
-            }
-            // Convertimos las horas de inicio y fin a formato de 24 horas si están en formato de 12 horas.
-            start_time = start_time.includes('AM') || start_time.includes('PM') ? convertTo24HourFormat(start_time) : start_time;
-            end_time = end_time.includes('AM') || end_time.includes('PM') ? convertTo24HourFormat(end_time) : end_time;
-
-            // Extraemos las horas de inicio y fin.
-            let startHour = convertTo12HourFormat(start_time);
-            let endHour = convertTo12HourFormat(end_time);
-
-            // Si el técnico trabajo hasta el día siguiente...
-            if (endHour < startHour) {
-                // Calculamos la tarifa para el primer día
-                let rateDay1 = calculateRate(date, start_time, '24:00', project.tarifa);
-                // guardar en la base de datos
-                await Tarea.create(
-                    { fecha: date, hora_inicio:startHour, hora_fin: '12:00 AM', total_hora: rateDay1.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rateDay1.isHoliday },
-                    { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
-                )
-                // Calculamos la tarifa para el día siguiente
-                let nextDay = new Date(date);
-                nextDay.setDate(nextDay.getDate() + 1);
-                nextDay = nextDay.toISOString().split('T')[0]
-                let rateDay2 = calculateRate(nextDay, '12:00 AM', end_time, project.tarifa);
-                // guardar en la base de datos
-                await Tarea.create(
-                    { fecha: nextDay, hora_inicio: '12:00 AM', hora_fin: endHour, total_hora: rateDay2.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rateDay2.isHoliday },
-                    { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
-                )
-                res.status(201).json({ message: 'Tareas creadas correctamente' })
-            } else {
-                // Si el técnico terminó el mismo día, calculamos la tarifa
-                const rate = calculateRate(date, start_time, end_time, project.tarifa)
-                // guardar en la base de datos
-                await Tarea.create(
-                    { fecha: date, hora_inicio:startHour, hora_fin:endHour, total_hora: rate.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rate.isHoliday },
-                    { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
-                    )
-                res.status(201).json({ message: 'Tarea creada correctamente' })
-            }
-        } catch (error) {
-            res.status(500).json({ message: error.message });
+// crear una tarea
+//NO TOCAR SI QUIERES HACER ALGO CONSULTA CON ROBERT :)
+static async create (req, res){
+    try {
+        // capturar datos
+        const { date, start_time, end_time, id_project, id_service } = req.body
+        // comprobar si existe el proyecto
+        const project = await Proyecto.findByPk(id_project)
+        if (!project) {
+            return res.status(404).json({message: 'Proyecto no encontrado'})
         }
+        // comprobar si existe el servicio
+        const serviceFound = await Servicio.findByPk(id_service)
+        if (!serviceFound) {
+            return res.status(404).json({message: 'Servicio no encontrado'})
+        }
+        //Comprobar la fecha y que esta no sea superior a la actual
+        const fechaFound = await date;
+        if (!fechaFound) {
+            return res.status(404).json({message: 'Fecha Invalida'});
+        }
+        // Convierte las dos fechas a milisegundos
+        let date1_ms = new Date(fechaFound).getTime();
+        let date2_ms = new Date().getTime();
+        // calculamos la diferecia
+        let difference_ms = date2_ms - date1_ms;
+        // Y si la da un resultado negativo la fecha esta en el futuro
+        if (difference_ms < 0) {
+            return res.status(400).json({message: 'La fecha no puede ser en el futuro'});
+        }
+
+        // Convertimos las horas de inicio y fin a formato de 24 horas si están en formato de 12 horas.
+        let converted_start_time = start_time.includes('AM') || start_time.includes('PM') ? convertTo24HourFormat(start_time) : start_time;
+        let converted_end_time = end_time.includes('AM') || end_time.includes('PM') ? convertTo24HourFormat(end_time) : end_time;
+
+        // Extraemos las horas de inicio y fin.
+        //let startHour = convertTo12HourFormat(converted_start_time);
+        //let endHour = convertTo12HourFormat(converted_end_time);
+
+        // Extraemos las horas de inicio y fin.
+        let startHour = converted_start_time;
+        let endHour = converted_end_time;
+
+        if (startHour === endHour) {
+            // Si la hora de inicio y la hora de fin son iguales, entonces el técnico trabajó 24 horas.
+            // Generamos dos registros, uno para cada día.
+            let rateDay1 = calculateRate(date, startHour, '24:00', project.tarifa);
+            await Tarea.create(
+                { fecha: date, hora_inicio:startHour, hora_fin: '24:00', total_hora: rateDay1.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rateDay1.isHoliday },
+                { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
+            )
+                
+            let nextDay = new Date(date);
+            nextDay.setDate(nextDay.getDate() + 1);
+            let rateDay2 = calculateRate(nextDay.toISOString().split('T')[0], '00:00', endHour, project.tarifa);
+            await Tarea.create(
+                { fecha: nextDay.toISOString().split('T')[0], hora_inicio: '00:00', hora_fin: endHour, total_hora: rateDay2.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rateDay2.isHoliday },
+                { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
+            )
+            res.status(201).json({ message: 'Tarea creada correctamente' })
+        } else {
+           // Si el técnico trabajo hasta el día siguiente...
+        if (endHour < startHour) {
+            // Calculamos la tarifa para el primer día
+            let rateDay1 = calculateRate(date, converted_start_time, '00:00', project.tarifa);
+            // guardar en la base de datos
+            await Tarea.create(
+                { fecha: date, hora_inicio:startHour, hora_fin: '00:00', total_hora: rateDay1.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rateDay1.isHoliday },
+                { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
+            )
+            // Calculamos la tarifa para el día siguiente
+            let nextDay = new Date(date);
+            nextDay.setDate(nextDay.getDate() + 1);
+            nextDay = nextDay.toISOString().split('T')[0]
+            let rateDay2 = calculateRate(nextDay, '00:00', converted_end_time, project.tarifa);
+            // guardar en la base de datos
+            await Tarea.create(
+                { fecha: nextDay, hora_inicio: '00:00', hora_fin: endHour, total_hora: rateDay2.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rateDay2.isHoliday },
+                { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
+            )
+            res.status(201).json({ message: 'Tareas creadas correctamente' })
+        } else {
+            // Si el técnico terminó el mismo día, calculamos la tarifa
+            const rate = calculateRate(date, converted_start_time, converted_end_time, project.tarifa)
+            // guardar en la base de datos
+            await Tarea.create(
+                { fecha: date, hora_inicio:startHour, hora_fin:endHour, total_hora: rate.totalHours, id_proyecto_fk: id_project, id_servicio_fk: id_service, feriado_fk: rate.isHoliday },
+                { fields: ['fecha', 'hora_inicio', 'hora_fin', 'total_hora', 'id_proyecto_fk', 'id_servicio_fk', 'feriado_fk'] }
+            )
+            res.status(201).json({ message: 'Tarea creada correctamente' })
+        }
+        }
+
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Hubo un problema al crear la tarea' })
     }
+}
 
     // hay que revisar esta funcion
     // actualizar una tarea
--- src/services/tarifa.js
+++ src/services/tarifa.js
@@ -1,33 +1,53 @@
 import holidayMockup from '../../feriados/model/HolidaysMockup.js'
 
+//NO TOCAR SI QUIERES HACER ALGO CONSULTA CON ROBERT :)
+
 //const holidays = ['2024-02-11', '2024-12-25'];
 const holidays = await holidayMockup.getHolidaysDate();
 
 export function calculateRate(startDate, startTime, endTime, hourlyRate) {
     // Convertimos la fecha de inicio a un objeto Date.
-    
+        //valor de las ganancias por minuto
+        let minuteRate = hourlyRate / 60
     let endDate = new Date(startDate);
-    // Extraemos las horas de inicio y fin de las cadenas de tiempo.
-    let startHour = parseInt(startTime.split(':')[0]);
-    let endHour = parseInt(endTime.split(':')[0]);
+        // Extraemos las horas y los minutos de inicio y fin de las cadenas de tiempo.
+        let startHour = parseInt(startTime.split(':')[0]);
+        let endHour = parseInt(endTime.split(':')[0]);
+        let startMinute = parseInt(startTime.split(':')[1]);
+        let endMinute = parseInt(endTime.split(':')[1]);
 
     // Si la hora de fin es menor que la hora de inicio, significa que el técnico trabajó hasta el día siguiente.
-    if (endHour < startHour) {
+    if (endHour <= startHour) {
         endDate.setDate(endDate.getDate() + 1);
     }
 
-    // Calculamos las horas totales trabajadas.
+    // Calculamos las horas y los minutos totales trabajados.
     let totalHours = endHour >= startHour ? endHour - startHour : 24 - startHour + endHour;
+    let totalMinutes = endMinute >= startMinute ? endMinute - startMinute : 60 - startMinute + endMinute;
+
+
     let dayHours = 0;
     let nightHours = 0;
 
-    // Si el técnico terminó el mismo día...
+    // Calculamos las horas totales trabajadas.
+    //si el tecnico trabjajo 24 horas
+    if ( endHour == startHour){
+        dayHours = 12
+        nightHours = 12
+        totalHours = 24
+    }
+    else{
+        totalHours = endHour >= startHour ? endHour - startHour : 24 - startHour + endHour;
+        dayHours = 0;
+        nightHours = 0;   
+         // Si el técnico terminó el mismo día...
     if (startHour < endHour) {
         // Calculamos las horas de día y de noche.
         dayHours = startHour < 19 && endHour > 7 ? Math.min(19, endHour) - Math.max(7, startHour) : 0;
         nightHours = totalHours - dayHours;
 
-    } else {
+    } 
+    else {
         // Si el técnico trabajó hasta el día siguiente...
         if (startHour < 19) {
             // Si el técnico comenzó a trabajar durante el día, calculamos las horas de día y de noche.
@@ -41,6 +61,7 @@ export function calculateRate(startDate, startTime, endTime, hourlyRate) {
             
         }
 
+    } 
     }
 
     // Determinamos si el día de trabajo es un fin de semana o un día festivo.
@@ -49,57 +70,68 @@ export function calculateRate(startDate, startTime, endTime, hourlyRate) {
     let isSaturday = dayOfWeek === 6;
     let isHoliday = holidays.includes(endDate.toISOString().split('T')[0]);
 
-    // Calculamos el multiplicador de la tarifa basándonos en si es un fin de semana o un día festivo.
+    // Calculamos el multiplicador de la tarifa
+    //Feriado y Domingo = 2
+    //Sabado=1.5
+
     let rateMultiplier = 1;
     let yes = 0;
-
-    if (isSunday & isHoliday) {
+    //si es domingo y feriado
+    if (isSunday && isHoliday) {
         rateMultiplier = 2;
         yes = 1
     }
-    else if (isSaturday & isHoliday) {
+    //si es sabado y feriado
+    else if (isSaturday && isHoliday) {
         rateMultiplier = 2;
         yes = 1
     }
+    //si es domingo
     else if (isSunday) {
         rateMultiplier = 2;
         yes = 1
     } 
+    //si es feriado
     else if (isHoliday) {
         rateMultiplier = 2;
         yes = 1
     } 
+    //si es sabado
     else if (isSaturday) {
         rateMultiplier = 1.5;
         yes = 1
     } 
+    //si es un dia normal
     else  {
         rateMultiplier = 1;
         yes = 0
-    }
+    } 
 
     // Devolvemos el total de la tarifa y las horas trabajadas.
     //yes es usado para saber si es un dia especial si no lo es las horas nocturnas se cobran extras
-        if (yes==1) {
-            return {
-                totalRate: (dayHours + nightHours) * hourlyRate * rateMultiplier,
-                totalHours: totalHours,
-                dayHours: dayHours,
-                nightHours: nightHours,
-                isHoliday: isHoliday,
-                dayOfWeek: dayOfWeek 
-            };
+    if (yes==1) {
+        return {
+            totalRate: (dayHours + nightHours) * hourlyRate + totalMinutes * minuteRate * rateMultiplier,
+            totalHours: totalHours,
+            dayHours: dayHours,
+            nightHours: nightHours,
+            isHoliday: isHoliday,
+            dayOfWeek: dayOfWeek,
+            totalMinutes: totalMinutes,
+            minuteRate: minuteRate,
+        };
+    }
+    else{ 
+        return {
+            totalRate: (dayHours + nightHours * 1.5) * hourlyRate + totalMinutes * minuteRate * rateMultiplier,
+            totalHours: totalHours,
+            dayHours: dayHours,
+            nightHours: nightHours,
+            isHoliday: isHoliday,
+            dayOfWeek: dayOfWeek,
+            totalMinutes: totalMinutes,
+            minuteRate: minuteRate,
         }
-        else { 
-            return {
-                totalRate: (dayHours + nightHours * 1.5) * hourlyRate * rateMultiplier,
-                totalHours: totalHours,
-                dayHours: dayHours,
-                nightHours: nightHours,
-                isHoliday: isHoliday,
-                dayOfWeek: dayOfWeek 
-            }
-        }
+    }
+
 }
-
-
