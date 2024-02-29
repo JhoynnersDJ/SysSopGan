156e6b58ee5ff4775cdc9c333555359f9c9519ad
Edouaard7 <136937565+Edouaard7@users.noreply.github.com>
2024-02-29 08:53:54 -0400

    Actualizar Tarea agregado

--- src/controllers/TareaController.js
+++ src/controllers/TareaController.js
@@ -169,24 +169,88 @@ static async create (req, res){
     }
 }
 
-    // hay que revisar esta funcion
+    //NO TOCAR SI QUIERES HACER ALGO CONSULTA CON ROBERT :)
     // actualizar una tarea
     static async update (req, res){
         try {
             // capturar datos
             const { id } = req.params
-            const { end_time, total_hours, id_holiday } = req.body
-            // comprobar si existe
+            const {start_time, end_time, id_project, id_service} = req.body
+            // comprobar si existe la tarea
             const tastkFound = await Tarea.findByPk(id)
             if (!tastkFound) {
                 return res.status(404).json({ message: 'Tarea no encontrada' })
             } 
-            // guardar a la base de datos
-            await Tarea.update(
-            { hora_fin: end_time, total_hora: total_hours, feriado_fk: id_holiday },
-            { where: { id_tarea: id } }
-            )
-            res.status(200).json({ message: 'Tarea actualizada correctamente' })
+            // comprobar si existe el proyecto
+            const project = await Proyecto.findByPk(id_project,{
+                
+                attributes: [
+                    'id_proyecto',
+                    'tarifa',
+                    'nombre_proyecto',
+                    'id_responsable_tecnico_fk',
+                    'id_usuario_fk',
+                    'id_responsable_cliente_fk',
+                    'status',
+                    'fecha_inicio'
+                ]
+            })
+            if (!project) {
+                return res.status(404).json({message: 'Proyecto no encontrado'})
+            }
+            // comprobar si existe el proyecto y en ese proyecto existe la tarea
+            const projectt = await Proyecto.findByPk(id_project,{
+                attributes: [
+                    'id_proyecto',
+                    'tarifa',
+                    'nombre_proyecto',
+                    'id_responsable_tecnico_fk',
+                    'id_usuario_fk',
+                    'id_responsable_cliente_fk',
+                    'status',
+                    'fecha_inicio'
+                ],
+                 where: {id_tarea: id },
+            })
+            if (!projectt) {
+                return res.status(404).json({message: 'Tarea no encontrado en ese Proyecto'})
+             }
+            // comprobar si existe el servicio
+            const serviceFound = await Servicio.findByPk(id_service)
+            if (!serviceFound) {
+                return res.status(404).json({message: 'Servicio no encontrado'})
+            }
+
+            // Convertimos las horas de inicio y fin a formato de 24 horas si están en formato de 12 horas.
+            let converted_start_time = start_time.includes('AM') || start_time.includes('PM') ? convertTo24HourFormat(start_time) : start_time;
+            let converted_end_time = end_time.includes('AM') || end_time.includes('PM') ? convertTo24HourFormat(end_time) : end_time;
+
+            // Extraemos las horas de inicio y fin.
+            //let startHour = convertTo12HourFormat(converted_start_time);
+            //let endHour = convertTo12HourFormat(converted_end_time);
+
+            // Extraemos las horas de inicio y fin.
+            let startHour = converted_start_time;
+            let endHour = converted_end_time;
+            if (startHour === endHour) {
+                res.status(400).json({ message: 'Mala colocacion de las horas verifique que las horas esten dentro del mismo dia' })
+            } 
+            else {
+               // Si el técnico trabajo hasta el día siguiente...
+                if (endHour < startHour) {
+                    res.status(400).json({ message: 'Mala colocacion de las horas verifique que las horas esten dentro del mismo dia' })
+                } else {
+                    // Si el técnico terminó el mismo día, calculamos la tarifa
+                    const rate = calculateRate(project.fecha_inicio ,converted_start_time, converted_end_time, project.tarifa)
+                    // guardar en la base de datos
+                    await Tarea.update(
+                        { hora_inicio:startHour, hora_fin:endHour, total_hora: rate.totalHours, id_servicio_fk: id_service, total_tarifa: rate.totalRate},
+                        { fields: ['hora_inicio', 'hora_fin', 'total_hora', 'id_servicio_fk', 'total_tarifa'] },
+                        { where: { id_tarea: id , id_proyecto_fk:id_project } }
+                    )
+                        res.status(201).json({ message: 'Tarea creada correctamente' })
+                }
+             }
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
